93d2272ac030466202e80c4a134fde80fc8bdc56
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-05-29 19:52:17 +0600

    adding css

--- src/Chats.js
+++ src/Chats.js
@@ -2,6 +2,7 @@ import { Avatar } from '@material-ui/core';
 import React from 'react';
 import "./Chats.css";
 import SearchIcon from '@material-ui/icons/Search';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 
 function Chats() {
     return (
@@ -10,10 +11,17 @@ function Chats() {
                 <Avatar className="chats_avatar"/>
                 <div className="chats_search">
                     <SearchIcon/>
+                    <input placeholder = "Find Friends" type="text" />
                 </div>
+                <ChatBubbleIcon className="chats_chatIcon" />
             </div>
+
+            <div className="chat_posts">
+                
+            </div>
+
         </div>
     )
 }
 
-export default Chats
+export default Chats;
