f0bddd337522dd5d9cdf79b79f651861f29e34a5
Tanjin Ahamed <tanjin.ahamed123@gmail.com>
2021-05-29 16:36:19 +0600

    Building the Preview Screen

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import WebcamCapture from './WebcamCapture';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
+import Preview from './Preview';
+import Chats from './Chats';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Tanjin, How are you? </h1>
       <Router>
-        <div>
+        <div className="app__body">
 
           <Switch>
-
-            <Route path="/">
-              <WebcamCapture />
+            <Route path="/chats">
+              <Chats />
+            </Route>
+            <Route path="/preview">
+              <Preview />
             </Route>
             <Route exact path="/">
               <WebcamCapture />
