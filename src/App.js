2688c37a671258dacb4063336bff12baa6d1e5d9
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-05-29 23:12:28 +0600

    chatview done

--- src/App.js
+++ src/App.js
@@ -8,17 +8,29 @@ import {
   Link
 } from "react-router-dom";
 import Chats from './Chats';
+import ChatView from './ChatView';
+import { selectUser } from './features/appSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import Login from './Login';
 
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+
   return (
     <div className="App">
       <h1>Tanjin, How are you? </h1>
       <Router>
-        <div>
-
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app_body">
           <Switch>
-            
+            <Route path="/chats/view">
+              <ChatView />
+            </Route>
             <Route path="/chats">
               <Chats />
             </Route>
@@ -30,6 +42,8 @@ function App() {
             </Route>
           </Switch>
         </div>
+        )}
+        
       </Router>
     </div>
   );
